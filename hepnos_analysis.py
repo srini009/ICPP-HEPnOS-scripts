fbb82102310e258b37b05837482b52f0b6758510
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-03-21 18:25:58 -0700

    Add more changes

--- hepnos_analysis.py
+++ hepnos_analysis.py
@@ -20,18 +20,22 @@ contents4 = sdskv_putpacked_latency.readlines()
 contents5 = sdskv_putpacked_num_entrants.readlines()
 contents6 = sdskv_putpacked_batch_size.readlines()
 
+model = regressionmodel.multiple_linear_regr_model()
+
 for line in range(0, len(contents3)):
 	size, time, _id = contents3[line].split(',')
 	latency, time, _id = contents4[line].split(',')
 	batch_size, time, _id = contents6[line].split(',')
-	threaddict[int(_id)] = (float(size), float(latency), float(batch_size))
+	threaddict[int(_id)] = (float(size), float(latency)*1000000.0, float(batch_size))
+	ideal_width = model.predict([[threaddict[int(_id)][2], threaddict[int(_id)][0]]])
+	#ideal_width = model(threaddict[int(_id)][2])
+	acc_pc = ((threaddict[int(_id)][1] - ideal_width[0])/threaddict[int(_id)][1])*100.0
+	print("predicted, actual latency, and acc_pc", ideal_width[0], threaddict[int(_id)][1], acc_pc)
 
-i = 0
+i = 2
 
-model = regressionmodel.model()
+model = regressionmodel.multiple_linear_regr_model()
 percent_improve = defaultdict(list)
-x = []
-y = []
 
 while i < len(contents3):
 	start_index = i
@@ -39,8 +43,8 @@ while i < len(contents3):
 	n, start_time, _id = contents5[i].split(',')
 	start_time = float(start_time)
 	end_time = float(start_time)
-	threadidlist = []
-	threadidlist.append(int(_id))
+	threadidlist = set()
+	threadidlist.add(int(_id))
 	max_num_entrants = float(n)
 	for line_ in range (i+1, len(contents5)):
 		num_entrants, time_, _id = contents5[line_].split(',')
@@ -48,28 +52,24 @@ while i < len(contents3):
 			end_index = line_ - 1
 			end_time = float(time_)
 			break
-		threadidlist.append(int(_id))
+		threadidlist.add(int(_id))
 		if float(num_entrants) > max_num_entrants:
 			max_num_entrants =  float(num_entrants)
-	
-	total_latency = end_time - start_time
+
+	#total_latency = end_time - start_time
+	total_latency = 0.0
 	total_data_size = 0.0
-	avg_bw = 0.0
 	for id_ in threadidlist:
 		total_data_size += threaddict[id_][0]
 
 	ideal_width = 0.0
 	for id_ in threadidlist:
-		per_kv_size = threaddict[id_][0]/threaddict[id_][2]
-		latency_per_kv_size = threaddict[id_][1]/threaddict[id_][2]
-		x.append(per_kv_size)
-		y.append(latency_per_kv_size)
-		latency_per_byte = latency_per_kv_size/per_kv_size
-		#print("Latency per byte ", latency_per_kv_size/per_kv_size)
-		ideal_width += threaddict[id_][2]*model(per_kv_size)
-	percentage_improvement = ((total_latency - ideal_width)/total_latency)*100.0
+		total_latency = threaddict[id_][1]
+		ideal_width += model.predict([[threaddict[id_][2], threaddict[id_][0]]])
+	#print("ID, Total Latency and Predicted: ", threadidlist, total_latency, ideal_width)
+	percentage_improvement = ((total_latency - ideal_width[0])/total_latency)*100.0
 	percent_improve[max_num_entrants].append(percentage_improvement)
-	print("Height, percentage improvement: ", max_num_entrants, percentage_improvement)
+	#print("Height, percentage improvement: ", max_num_entrants, percentage_improvement)
 	i = end_index + 1
 
 #plt.plot(x)
--- hepnos_regression.py
+++ hepnos_regression.py
@@ -8,27 +8,33 @@ from sklearn import linear_model
   
 #pid = str(sys.argv[1])
 
-def model():
-	pid = "205993"
-	sdskv_putpacked_size = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_data_size_"+pid+"_0","r")
-	sdskv_putpacked_batch_size = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_batch_size_"+pid+"_0","r")
-	sdskv_putpacked_latency = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_latency_"+pid+"_0","r")
-	contents1 = sdskv_putpacked_size.readlines()
-	contents2 = sdskv_putpacked_latency.readlines()
-	contents3 = sdskv_putpacked_batch_size.readlines()
-	csv_file = open("putpacked.csv", "w")
-
+def multiple_linear_regr_model():
 	raw_sizes = []
 	raw_batch_sizes = []
 	raw_latencies = []
-	for line in range(0, len(contents1)):
-		size, time, _id = contents1[line].split(',')
-		latency, time, _id = contents2[line].split(',')
-		batch_size, time, _id = contents3[line].split(',')
-		raw_sizes.append(float(size))
-		raw_batch_sizes.append(float(batch_size))
-		raw_latencies.append(float(latency))
 
+	pidlist = ["21961", "43194", "137079"]
+
+	for pid in pidlist:
+		sdskv_putpacked_size = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_data_size_"+pid+"_0","r")
+		sdskv_putpacked_batch_size = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_batch_size_"+pid+"_0","r")
+		sdskv_putpacked_latency = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_latency_"+pid+"_0","r")
+	#sdskv_putpacked_size = open("/home/srinivasan/Documents/MOCHI/hist/sdskv_putpacked_data_size_"+pid+"_4","r")
+	#sdskv_putpacked_batch_size = open("/home/srinivasan/Documents/MOCHI/hist/sdskv_putpacked_batch_size_"+pid+"_4","r")
+	#sdskv_putpacked_latency = open("/home/srinivasan/Documents/MOCHI/hist/sdskv_putpacked_latency_"+pid+"_4","r")
+		contents1 = sdskv_putpacked_size.readlines()
+		contents2 = sdskv_putpacked_latency.readlines()
+		contents3 = sdskv_putpacked_batch_size.readlines()
+
+		for line in range(0, len(contents1)):
+			size, time, _id = contents1[line].split(',')
+			latency, time, _id = contents2[line].split(',')
+			batch_size, time, _id = contents3[line].split(',')
+			raw_sizes.append(float(size))
+			raw_batch_sizes.append(float(batch_size))
+			raw_latencies.append(float(latency)*1000000.0)
+
+	csv_file = open("putpacked.csv", "w")
 	csv_file.write("BatchSize,DataSize,Latency\n")
 	for elem in range(0, len(raw_sizes)):
 		csv_file.write(str(raw_batch_sizes[elem])+","+str(raw_sizes[elem])+","+str(raw_latencies[elem])+"\n")
@@ -39,10 +45,32 @@ def model():
 	y = df['Latency']
 	regr = linear_model.LinearRegression()
 	regr.fit(X, y)
-	#return mymodel
-	#myline = np.linspace(1, 1000000, 100)
-	#fig, ax = plt.subplots(figsize =(10, 7))
-	#x.scatter(np.array(raw_sizes), np.array(raw_latencies)) 
-	#x.plot(myline, mymodel(myline))
-	# Show plot 
-	#lt.show() 
+	return regr
+
+def linear_regr_model():
+	pid = "21961"
+	sdskv_putpacked_size = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_data_size_"+pid+"_0","r")
+	sdskv_putpacked_batch_size = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_batch_size_"+pid+"_0","r")
+	sdskv_putpacked_latency = open("/home/srinivasan/Documents/MOCHI/hist/theta_sdskv_benchmarks/sdskv_putpacked_latency_"+pid+"_0","r")
+	#sdskv_putpacked_size = open("/home/srinivasan/Documents/MOCHI/hist/sdskv_putpacked_data_size_"+pid+"_4","r")
+	#sdskv_putpacked_batch_size = open("/home/srinivasan/Documents/MOCHI/hist/sdskv_putpacked_batch_size_"+pid+"_4","r")
+	#sdskv_putpacked_latency = open("/home/srinivasan/Documents/MOCHI/hist/sdskv_putpacked_latency_"+pid+"_4","r")
+	contents1 = sdskv_putpacked_size.readlines()
+	contents2 = sdskv_putpacked_latency.readlines()
+	contents3 = sdskv_putpacked_batch_size.readlines()
+
+	raw_sizes = []
+	raw_batch_sizes = []
+	raw_latencies = []
+	for line in range(0, len(contents1)):
+		size, time, _id = contents1[line].split(',')
+		latency, time, _id = contents2[line].split(',')
+		batch_size, time, _id = contents3[line].split(',')
+		raw_sizes.append(float(size))
+		raw_batch_sizes.append(float(batch_size))
+		raw_latencies.append(float(latency)*1000000.0)
+	regr = np.poly1d(np.polyfit(raw_batch_sizes, raw_latencies, 1))
+	return regr
+
+
+multiple_linear_regr_model()
